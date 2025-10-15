a4e257ccfecf167b03730530a2730b56ac8adce2
Sergio <santossergiorealbjj@outlook.com>
2025-10-15 08:14:38 -0300

    Create DonationServiceTests.cs

---
+++ tests/VaquinhaOnline.UnitTests/Services/DonationServiceTests.cs
@@ -0,0 +1,40 @@
+using Xunit;
+using Moq;
+using VaquinhaOnline.Services;
+using VaquinhaOnline.Repositories;
+using VaquinhaOnline.Models;
+
+namespace VaquinhaOnline.UnitTests.Services
+{
+    public class DonationServiceTests
+    {
+        private readonly DonationService _service;
+        private readonly Mock<IDonationRepository> _repositoryMock;
+
+        public DonationServiceTests()
+        {
+            _repositoryMock = new Mock<IDonationRepository>();
+            _service = new DonationService(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public void Should_Add_Donation_When_Valid()
+        {
+            // Arrange
+            var donation = new Donation { Name = "Sergio", Amount = 100 };
+
+            // Act
+            _service.ProcessDonation(donation);
+
+            // Assert
+            _repositoryMock.Verify(r => r.Add(donation), Times.Once);
+        }
+
+        [Fact]
+        public void Should_Throw_Exception_When_Donation_Is_Null()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => _service.ProcessDonation(null));
+        }
+    }
+}
